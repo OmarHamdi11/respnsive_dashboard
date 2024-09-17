cb18b8592f7ee1b5ee7b5d7b3b5d1f5a0ef1a249
OmarHamd11 <omarellafy1@gmail.com>
2024-09-17 17:06:45 +0300

    adding adaptive layout widget and desktop layout

---
+++ lib/views/dashboard_view.dart
@@ -0,0 +1,18 @@
+import 'package:dashboard/widgets/adaptive_layout.dart';
+import 'package:dashboard/widgets/desktop_layout.dart';
+import 'package:flutter/material.dart';
+
+class DashboardView extends StatelessWidget {
+  const DashboardView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AdaptiveLayout(
+        mobileLayout: (context) => const SizedBox(),
+        tabletLayout: (context) => const SizedBox(),
+        desktopLayout: (context) => const DesktopLayout(),
+      ),
+    );
+  }
+}
