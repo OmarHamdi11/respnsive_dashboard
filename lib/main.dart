7f495c3436de64d15a4100a66bb8cce22950b0c1
OmarHamd11 <omarellafy1@gmail.com>
2024-09-17 22:24:30 +0300

    adding image assets, font assets and text styles

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DashboardView(),
+      theme: ThemeData(fontFamily: 'Montserrat'),
+      home: const DashboardView(),
     );
   }
 }
