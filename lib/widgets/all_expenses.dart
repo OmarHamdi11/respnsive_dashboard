930b970588f869a0e210d930ecbff93b5e85ba29
OmarHamd11 <omarellafy1@gmail.com>
2024-09-18 18:59:58 +0300

    builed all expenses header ui and all expenses item ui

---
+++ lib/widgets/all_expenses.dart
@@ -0,0 +1,35 @@
+import 'package:dashboard/models/all_expenses_item_model.dart';
+import 'package:dashboard/utils/app_images.dart';
+import 'package:dashboard/widgets/all_expenses_header.dart';
+import 'package:dashboard/widgets/all_expenses_item.dart';
+import 'package:flutter/material.dart';
+
+class AllExpenses extends StatelessWidget {
+  const AllExpenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      child: const Column(
+        children: [
+          AllExpensesHeader(),
+          SizedBox(height: 16),
+          AllExpensesItem(
+              itemModel: AllExpensesItemModel(
+            image: Assets.imagesIncome,
+            title: 'Income',
+            date: 'April 2022',
+            price: r'$20,129',
+          ))
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/all_expenses_header.dart
@@ -0,0 +1,23 @@
+import 'package:dashboard/utils/app_styles.dart';
+import 'package:dashboard/widgets/range_options.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AllExpensesHeader extends StatelessWidget {
+  const AllExpensesHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [
+        Text(
+          'All Expenses',
+          style: AppStyles.styleSemiBold20,
+        ),
+        Expanded(child: SizedBox()),
+        RangeOptions()
+      ],
+    );
+  }
+}
